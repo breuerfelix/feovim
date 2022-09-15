f8c17dfc71f290c5005ed0d61f0f2e03c31db431
Felix Breuer <fbreuer@pm.me>
2022-09-15 22:51:26 +0200

    add plugins and make binaries available in path
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- flake.nix
+++ flake.nix
@@ -6,12 +6,29 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     # define plugin sources here
+    "which-key" = { url = "github:folke/which-key.nvim"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        extraPackages = with pkgs; [
+          #nixfmt
+        ];
+
+        startPlugins = [
+          "which-key"
+        ];
+
+        optPlugins = [ ];
+
         pkgs = nixpkgs.legacyPackages.${system};
+        # installs a vim plugin from git
+        plugin = repo: pkgs.vimUtils.buildVimPluginFrom2Nix {
+          pname = "${pkgs.lib.strings.sanitizeDerivationName repo}";
+          version = "main";
+          src = builtins.getAttr repo inputs;
+        };
       in
       {
         apps = rec {
@@ -27,8 +44,16 @@
           feovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
             viAlias = true;
             vimAlias = true;
+            #withNodesJs = true; # TODO why is this one unexpected?
+            withPython3 = true;
+            withRuby = true;
+            extraMakeWrapperArgs = ''--prefix PATH : "${pkgs.lib.makeBinPath extraPackages}"'';
             configure = {
               customRC = "";
+              packages.myVimPackage = {
+                start = map (name: (plugin name)) startPlugins;
+                opt = map (name: (plugin name)) optPlugins;
+              };
             };
           };
         };
