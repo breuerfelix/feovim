2ba3c4534e120040bdd9ddcfea0fd36d0ad2476d
Felix Breuer <fbreuer@pm.me>
2022-09-16 10:03:30 +0200

    add sources

--- flake.nix
+++ flake.nix
@@ -12,14 +12,39 @@
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        # define packages that need to be available in the neovim path
         extraPackages = with pkgs; [
-          #nixfmt
+          tree-sitter
+          jq
+          curl # rest.nvim
+          bat
+          ripgrep
+          fd
+          fzf # fzf
+          nodejs # github copilot
+          universal-ctags # vista
+
+          # extra language servers
+          rnix-lsp
+          #terraform-ls terraform-lsp # TODO fix
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          gopls
+          texlab
+          nodePackages.pyright
+          black
+          rust-analyzer
+
+          # debugging
+          delve # golang
         ];
 
+        # plugins loaded at start
         startPlugins = [
           "which-key"
         ];
 
+        # plugins loaded optionally
         optPlugins = [ ];
 
         pkgs = nixpkgs.legacyPackages.${system};
@@ -39,17 +64,29 @@
           };
         };
 
-        packages = rec {
+        packages = with pkgs; rec {
           default = feovim;
-          feovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
+          feovim = wrapNeovim neovim-unwrapped {
             viAlias = true;
             vimAlias = true;
             #withNodesJs = true; # TODO why is this one unexpected?
             withPython3 = true;
             withRuby = true;
-            extraMakeWrapperArgs = ''--prefix PATH : "${pkgs.lib.makeBinPath extraPackages}"'';
+            extraMakeWrapperArgs = ''--prefix PATH : "${lib.makeBinPath extraPackages}"'';
             configure = {
-              customRC = "";
+              # import your individual vim config files here
+              customRC = builtins.concatStringsSep "\n" [
+                (lib.strings.fileContents ./base.vim)
+                (lib.strings.fileContents ./plugins.vim)
+                (lib.strings.fileContents ./lsp.vim)
+                ''
+                  lua << EOF
+                  ${lib.strings.fileContents ./config.lua}
+                  ${lib.strings.fileContents ./lsp.lua}
+                  ${lib.strings.fileContents ./debug.lua}
+                  EOF
+                ''
+              ];
               packages.myVimPackage = {
                 start = map (name: (plugin name)) startPlugins;
                 opt = map (name: (plugin name)) optPlugins;
