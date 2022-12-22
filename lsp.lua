8c05f2087b811d47942230cf4e94d672aa33721a
Felix Breuer <fbreuer@pm.me>
2022-12-22 15:28:46 +0100

    revert plugins
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- lsp.lua
+++ lsp.lua
@@ -109,7 +109,7 @@ local servers = {
 }
 
 local caps = vim.lsp.protocol.make_client_capabilities()
-local capabilities = require('cmp_nvim_lsp').default_capabilities(caps)
+local capabilities = require('cmp_nvim_lsp').update_capabilities(caps)
 
 for key, value in pairs(servers) do
   lspconfig[key].setup {
