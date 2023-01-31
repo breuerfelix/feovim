2098aae32bbc9ecac0c0cf914f2f011faa1209cb
Felix Breuer <fbreuer@pm.me>
2023-01-31 22:48:33 +0100

    prettify
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- plugins.lua
+++ plugins.lua
@@ -43,7 +43,7 @@ require('fzf-lua').setup {
 require('gitsigns').setup {
   signs = {
     -- source: https://en.wikipedia.org/wiki/Box-drawing_character
-    add          = { hl = 'GitSignsAdd', text = '┃', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+    add          = { hl = 'GitSignsAdd'   , text = '┃', numhl = 'GitSignsAddNr'   , linehl = 'GitSignsAddLn'    },
     change       = { hl = 'GitSignsChange', text = '┃', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
     delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
     topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
