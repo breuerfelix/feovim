89169b1a3e15300fbc1ada79df87063a2203a26c
Felix Breuer <fbreuer@pm.me>
2024-11-03 22:12:45 +0100

    chore: fix tree closing
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- tree.nix
+++ tree.nix
@@ -13,7 +13,29 @@
           -- set colors
           vim.opt.termguicolors = true
 
+          local function on_attach(bufnr)
+            local api = require "nvim-tree.api"
+
+            local function opts(desc)
+              return {
+                desc = "nvim-tree: " .. desc,
+                buffer = bufnr,
+                noremap = true,
+                silent = true,
+                nowait = true,
+              }
+            end
+
+            -- default mappings
+            api.config.mappings.default_on_attach(bufnr)
+
+            vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+            -- collides with closing vim keybind
+            vim.keymap.set("n", "<C-e>", "<cmd>:q<cr>", opts("Quit"))
+          end
+
           require('nvim-tree').setup {
+            on_attach = on_attach,
             sync_root_with_cwd = true,
             git = {
               ignore = true,
